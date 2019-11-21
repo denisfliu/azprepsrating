fc4881cc6b9e8aa1bee434863966707483db780d
denisfliu <denisliu10@gmail.com>
2019-11-21 09:21:52 -0700

    incorporated multiple individual collection

--- individualcollect.py
+++ individualcollect.py
@@ -50,37 +50,41 @@ def log_error(e):
     print(e)
 
 if __name__ == "__main__":
-    print('Enter individual url: ')
-    url = input()
-    raw_html = simple_get(url)
-    html = BeautifulSoup(raw_html, 'html.parser')
-    player = html.find('h4', class_ = 'title is-3').get_text().strip()
-    a = []
-    b = []
-    for i in html.find_all(lambda tag: tag.name == 'div' and 
-                                   tag.get('class') == ['card']):
-        dubs = get_doubles_partner(i.find('div', class_ = 'card-header-title').get_text().strip())
-        for j in i.find_all(lambda tag: tag.name == 'div' and 
-                                   tag.get('class') == ['column']):
-            a_tags = j.select('a')
-            a.append(dubs + ', ' + ', '.join([k.get_text().strip().replace('											', '').replace('\n', ' ') for k in a_tags[:len(a_tags) - 1]]))
-        for j in i.find_all('div', class_ = 'column is-3 result'):
-            b.append(j.get_text().strip())
-    
-    '''
-    for i, p in enumerate(html.select('p')):
-        a.append(p.text)  
-    del a[0:16]
-    scores = []
-    for i in html.find_all('div', class_ = 'column is-3 result'):
-        scores.append(i.get_text())
-    index = 0
-    for i in range(0, len(a)):
-        if i % 4 == 3:
-            a.insert(i, scores[index])
-            index = index + 1
-    '''
-    with open('data.csv', 'w') as csvfile:
-        for name, score in zip(a, b):
-            score = score.replace('-', ',')
-            csvfile.write(f'{player}, {name}, {score}\n')  
+    while True:
+        print('Enter individual url or enter 0 to stop: ')
+        url = input()
+        if url == '0':
+            break
+        else:
+            raw_html = simple_get(url)
+            html = BeautifulSoup(raw_html, 'html.parser')
+            player = html.find('h4', class_ = 'title is-3').get_text().strip()
+            a = []
+            b = []
+            for i in html.find_all(lambda tag: tag.name == 'div' and 
+                                        tag.get('class') == ['card']):
+                dubs = get_doubles_partner(i.find('div', class_ = 'card-header-title').get_text().strip())
+                for j in i.find_all(lambda tag: tag.name == 'div' and 
+                                        tag.get('class') == ['column']):
+                    a_tags = j.select('a')
+                    a.append(dubs + ', ' + ', '.join([k.get_text().strip().replace('											', '').replace('\n', ' ') for k in a_tags[:len(a_tags) - 1]]))
+                for j in i.find_all('div', class_ = 'column is-3 result'):
+                    b.append(j.get_text().strip())
+            
+            '''
+            for i, p in enumerate(html.select('p')):
+                a.append(p.text)  
+            del a[0:16]
+            scores = []
+            for i in html.find_all('div', class_ = 'column is-3 result'):
+                scores.append(i.get_text())
+            index = 0
+            for i in range(0, len(a)):
+                if i % 4 == 3:
+                    a.insert(i, scores[index])
+                    index = index + 1
+            '''
+            with open('data.csv', 'a') as csvfile:
+                for name, score in zip(a, b):
+                    score = score.replace('-', ',')
+                    csvfile.write(f'{player}, {name}, {score}\n')  
